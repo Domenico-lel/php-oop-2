c3dbf83db20c522ef6dc53690b74202051297600
Domenico-lel <domenicolella2@gmail.com>
2024-02-05 17:52:31 +0100

    creazioni classi php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+</head>
+<body>
+    <?php
+
+   
+    class Prodotto {
+        
+    }
+
+    
+    class Carrello {
+        
+    }
+
+    
+    class Categoria {
+        
+    }
+    
+    class Cliente {
+        
+    }
+
+   
+    class Shop {
+        
+    }
+
+    
+    class Card {
+        
+    }
+
+    
+    class Ordine {
+        
+    }
+
+   
+    class Pagamento {
+        
+    }
+
+    ?>
+</body>
+</html>
