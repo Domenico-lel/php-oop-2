e3651baf0a881c9a12d4d53a1fd9f6e442df09c1
Domenico-lel <domenicolella2@gmail.com>
2024-02-05 18:05:01 +0100

    upp ordini - carrellol

--- index.php
+++ index.php
@@ -1,52 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop</title>
 </head>
+
 <body>
     <?php
 
-   
-    class Prodotto {
-        
+
+    class Prodotto
+    {
     }
 
-    
-    class Carrello {
-        
+
+    class Carrello
+    {
+        public function aggiungiProdotto(Prodotto $prodotto)
+        {
+            $this->prodotti[] = $prodotto;
+            $this->totale += $prodotto->prezzo;
+        }
     }
 
-    
-    class Categoria {
-        
-    }
-    
-    class Cliente {
-        
+
+    class Categoria
+    {
     }
 
-   
-    class Shop {
-        
+    class Cliente
+    {
     }
 
-    
-    class Card {
-        
+
+    class Shop
+    {
     }
 
-    
-    class Ordine {
-        
+
+    class Card
+    {
     }
 
-   
-    class Pagamento {
-        
+
+    class Ordine
+    {
+        public function aggiungiProdotto(prodotto $prodotto)
+        {
+            $this->prodottiOrdinati[] = $prodotto;
+            $this->totaleOrdine += $prodotto->prezzo;
+        }
+    }
+
+
+    class Pagamento
+    {
     }
 
     ?>
 </body>
+
 </html>
