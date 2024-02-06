d9b9617971d8c53f8de22ef13688ece8b276b280
Domenico-lel <domenicolella2@gmail.com>
2024-02-06 18:18:05 +0100

    punto di partenza giov.

--- index.php
+++ index.php
@@ -4,74 +4,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop</title>
+    <title>AnimalCommerce</title>
+
+    <?php require_once(__DIR__ . "/db.php"); ?>
 </head>
 
 <body>
-    <?php
 
+    <ul>
+        <?php
+        foreach ($prods as $prod) {
 
-    class Prodotto
-    {
-        public $nome;
-        public $prezzo;
-        public $categoria;
-        public $tipo;
+        ?>
 
-        public function __construct($nome, $prezzo, $categoria, $tipo)
-        {
-            $this->nome = $nome;
-            $this->prezzo = $prezzo;
-            $this->categoria = $categoria;
-            $this->tipo = $tipo;
+            <li>
+                <h3>
+                    <?php
+                    echo $prod->getTypology();
+                    ?>
+                </h3>
+                <?php
+                echo $prod->getTitle();
+                ?>:
+                <?php
+                echo $prod->getPrice();
+                ?>
+                Euro
+                <br>
+                <img src="<?php echo $prod->getImage() ?>" width="100" />
+                <br>
+                Category:
+                <img src="<?php echo $prod->getCategory()->getIcon() ?>" width="30" />
+                <?php
+                echo $prod->getCategory()->getName();
+                ?>
+            </li>
+
+        <?php
         }
-    }
-
-
-    class Carrello
-    {
-        public function aggiungiProdotto(Prodotto $prodotto)
-        {
-            $this->prodotti[] = $prodotto;
-            $this->totale += $prodotto->prezzo;
-        }
-    }
-
-
-    class Categoria
-    {
-    }
-
-    class Cliente
-    {
-    }
-
-
-    class Shop
-    {
-    }
-
-
-    class Card
-    {
-    }
-
-
-    class Ordine
-    {
-        public function aggiungiProdotto(prodotto $prodotto)
-        {
-            $this->prodottiOrdinati[] = $prodotto;
-            $this->totaleOrdine += $prodotto->prezzo;
-        }
-    }
-
-
-    class Pagamento
-    {
-    }
-
-    ?>
+        ?>
+    </ul>
 </body>
 
 </html>
